a3353e5f8f5e5504559bfe2d8af8282bde17f940
Sehajpal2013 <147245200+Sehajpal2013@users.noreply.github.com>
2024-06-05 21:00:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".btn").click(function(){                     
+    $("a").attr("href", "./destinazioni.html");
+});
+$(".btn1").click(function(){
+    $("a").attr("href", "./index.html");
+});
+
+
